c3f99baaac08448e1d82eaac3b1f585cbbfc92ef
maburtobi <53486725+maburtobi@users.noreply.github.com>
2021-10-30 02:06:15 +0000

    update
    print dynamix sysArch and add docker-compose install command

--- install-docker-debian.sh
+++ install-docker-debian.sh
@@ -13,10 +13,16 @@ sudo apt-get install \
     curl \
     gnupg \
     lsb-release
+
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+echo "This will install Docker-compose to the system"
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
