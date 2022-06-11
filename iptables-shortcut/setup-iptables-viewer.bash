7f1a4fac8a40d966694eaa11f5a7221484c9dde5
maburtobi <53486725+maburtobi@users.noreply.github.com>
2022-06-11 13:26:12 +0000

    Improve stability

--- iptables-shortcut/setup-iptables-viewer.bash
+++ iptables-shortcut/setup-iptables-viewer.bash
@@ -1,27 +1,32 @@
 #!/bin/bash
 #This will add su command for checking iptable firewall rules
 
+
 echo -e "START\n"
 echo "--------------------------------------"
 echo "Updating repository"
 sudo apt update
 
+
 echo "--------------------------------------"
 echo "Installing required packages"
 sudo apt install iptables iptables-persistent
 
+
 echo "--------------------------------------"
 echo "Installing command shortcut"
-sudo bash ip6tnum.bash
-sudo bash ip6tnum.bash
-sudo bash ip6tsave.bash
-sudo bash ipt.bash
-sudo bash iptnum.bash
-sudo bash iptsave.bash
+cd
+wget https://raw.githubusercontent.com/maburtobi/automations/main/iptables-shortcut/{ip6t,ip6tnum,ip6tsave,ipt,iptnum,iptsave}.bash
+sudo bash {ip6t,ip6tnum,ip6tsave,ipt,iptnum,iptsave}.bash
+rm {ip6t,ip6tnum,ip6tsave,ipt,iptnum,iptsave}.bash
 
+
+echo "--------------------------------------"
+echo "Make these accessible"
 cd /usr/local/sbin
 sudo chmod 750 ip6t ip6tnum ip6tsave ipt iptnum iptsave
 cd
+
 
 echo -e "---------------------------------\n\
 Iptables viewer command's setup has been successfully installed.\n\
